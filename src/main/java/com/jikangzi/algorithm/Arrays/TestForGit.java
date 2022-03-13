4ef2dedbf94f17f89d9d5f972670247a7ed1c96b
jikang <1198739493@qq.com>
2022-03-13 22:33:57 +0800

    test for git

---
+++ src/main/java/com/jikangzi/algorithm/Arrays/TestForGit.java
@@ -0,0 +1,13 @@
+package com.jikangzi.algorithm.Arrays;
+
+/**
+ * @author: jikang
+ * @description:
+ * @date: 2022/3/13 22:26
+ * @Modified By:
+ */
+public class TestForGit {
+    public static void main(String args[]){
+        System.out.println("慧敏");
+    }
+}
